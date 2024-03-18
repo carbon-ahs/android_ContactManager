3aade7a5731f5c3d56173cfed37a4bee5b374fdb
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-03-18 15:11:51 +0600

    listview done

--- app/src/main/java/com/axiagroups/contactmanager/MainActivity.java
+++ app/src/main/java/com/axiagroups/contactmanager/MainActivity.java
@@ -2,6 +2,11 @@ package com.axiagroups.contactmanager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +17,13 @@ import androidx.core.view.WindowInsetsCompat;
 import com.axiagroups.contactmanager.data.DatabaseHandler;
 import com.axiagroups.contactmanager.model.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayList<String> contactArrayList;
+    private ArrayAdapter<String> contactArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +40,42 @@ public class MainActivity extends AppCompatActivity {
 //        db.addContact(new Contact("Shehanuk", "01755215173"));
 //        db.addContact(new Contact("Jodu", "01755215111"));
 //        db.addContact(new Contact("Modu", "01755215222"));
+//        db.addContact(new Contact("Shakil", "0111"));
+//        db.addContact(new Contact("Tusar", "0122"));
+//        db.addContact(new Contact("Sorowar", "0133"));
+//        db.addContact(new Contact("Masum", "0144"));
+//        db.addContact(new Contact("billa", "0155"));
+//        db.addContact(new Contact("Ahsan", "0166"));
+//        db.addContact(new Contact("kodu", "0177"));
 
+        listView = findViewById(R.id.listView);
+        contactArrayList = new ArrayList<>();
         List<Contact> contactList = db.getAllContacts();
 
         for (Contact contact: contactList) {
             Log.d("TAG", "onCreate: " + contact.getName());
+            contactArrayList.add(contact.getName());
         }
+
+        //create array adapter
+        contactArrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                contactArrayList
+        );
+        // add to listview
+        listView.setAdapter(contactArrayAdapter);
+
+        // add event Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String clickedItem = listView.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this,clickedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 }
