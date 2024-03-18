cdd6c3182e9f5aff6bedeb5148d077cab923af8a
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-03-18 12:23:44 +0600

    create, read complite

--- app/src/main/java/com/axiagroups/contactmanager/MainActivity.java
+++ app/src/main/java/com/axiagroups/contactmanager/MainActivity.java
@@ -1,12 +1,18 @@
 package com.axiagroups.contactmanager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.axiagroups.contactmanager.data.DatabaseHandler;
+import com.axiagroups.contactmanager.model.Contact;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        DatabaseHandler db = new DatabaseHandler(this);
+//        db.addContact(new Contact("Shehanuk", "01755215173"));
+//        db.addContact(new Contact("Jodu", "01755215111"));
+//        db.addContact(new Contact("Modu", "01755215222"));
+
+        List<Contact> contactList = db.getAllContacts();
+
+        for (Contact contact: contactList) {
+            Log.d("TAG", "onCreate: " + contact.getName());
+        }
     }
 }
